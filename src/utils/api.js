a0d41aaa92b0a460f7902b4e4f0b45a04e926241
naseemsuleiman <naseemsuleiman08@gmail.com>
2026-01-30 23:18:20 +0300

    Update API base URL to remove '/api' suffix

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://metamind-backend-5tu9.onrender.com/api",
+  baseURL: "https://metamind-backend-5tu9.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
